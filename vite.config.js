b58f76f9560315ed23b698c9f313ab174dd4afd9
Shourya <shouryasinha.c@gmail.com>
2025-09-10 20:05:37 +0700

    modify vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,28 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+const repoName = "bhawani_construction_admin_panel";
+export default defineConfig(({ mode }) => ({
+  base: '/',
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+    build: {
+    outDir: 'dist',
+  },
+}));
