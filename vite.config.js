9c2cf2fa01a0c388c51d8e442c4aab736e0cd32e
Shourya-Sinha <shouryasinha.c@gmail.com>
2026-03-16 09:58:38 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 // export default defineConfig({
@@ -15,8 +16,8 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    // mode === 'development' &&
+    // componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
